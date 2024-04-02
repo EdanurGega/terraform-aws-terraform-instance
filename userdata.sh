a21fb36f118746d481b13b91b1523cc087f8cfa2
edanur <edanuraslanyurek@gmail.com>
2024-04-02 18:07:05 +0300

    please enter your key name

---
+++ userdata.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+hostnamectl set-hostname ${myserver}
+dnf update -y
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum -y install terraform
